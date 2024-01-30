32fe0b44068d51111c2d1690dd0e5399f48d1bb7
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-01-30 14:40:40 -0300

    Chat em tempo real e envio de dados acontecendo

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -2,21 +2,25 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/ChatContext';
 
-export default function Message({message}) {
+export default function Message({ message }) {
 
-  const {currentUser} = useContext(AuthContext);
-  const {data} = useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
 
   return (
-    <div className="message owner">
-{/*         <div className="messageInfo">
-            <img src="https://images.pexels.com/photos/1486213/pexels-photo-1486213.jpeg?auto=compress&cs=tinysrgb&w=600" />
-            <span>just now</span>
-        </div>
-        <div className="messageContent">
-            <p>Olá</p>
-             <img src="https://images.pexels.com/photos/1486213/pexels-photo-1486213.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" /> 
-        </div> */}
+    <div className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+      <div className="messageInfo">
+        <img src={
+          message.senderId === currentUser.uid
+            ? currentUser.photoURL
+            : data.user.photoURL
+        } />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div>
     </div>
 
   )
