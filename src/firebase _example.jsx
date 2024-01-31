b72246fe84bb77c1882fe54c6743cbbefb9c7677
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-01-31 17:37:49 -0300

    Retirando firebase do versionamento e subindo arquivo de exemplo

--- src/firebase _example.jsx
+++ src/firebase _example.jsx
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUkVObgDeaECAgWE-GA4s-6J689dJptRA",
-  authDomain: "chat-b66f4.firebaseapp.com",
-  projectId: "chat-b66f4",
-  storageBucket: "chat-b66f4.appspot.com",
-  messagingSenderId: "789788422799",
-  appId: "1:789788422799:web:01b53deda0e74d4fd636fa"
+  apiKey: "enter-your-data",
+  authDomain: "enter-your-data",
+  projectId: "enter-your-data",
+  storageBucket: "enter-your-data",
+  messagingSenderId: "enter-your-data",
+  appId: "enter-your-data"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
